0d40d3b69bb9b81f45403c35e6be189b51868c5a
kouhei.toda <0vr3252c3xv197v@gmail.com>
2018-10-31 09:39:54 +0000

    login

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,21 +8,4 @@ use App\User; // 追加
 
 class UsersController extends Controller
 {
-    public function index()
-    {
-        $users = User::paginate(10);
-
-        return view('users.index', [
-            'users' => $users,
-        ]);
-    }
-    
-    public function show($id)
-    {
-        $user = User::find($id);
-
-        return view('users.show', [
-            'user' => $user,
-        ]);
-    }
 }
